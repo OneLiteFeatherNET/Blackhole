4fb2a03716ea376ad6d528eab6e9ed8773f460e1
theEvilReaper <steffenwx@gmail.com>
2024-12-26 17:14:13 +0100

    Remove old implementation

--- backend/src/main/kotlin/net/onelitefeather/blackhole/spec/models/InjuryType.kt
+++
@@ -1,7 +0,0 @@
-package net.onelitefeather.blackhole.spec.models
-
-enum class InjuryType {
-
-    CHAT, NETWORK
-
-}
--- backend/src/main/kotlin/net/onelitefeather/blackhole/util/Constants.kt
+++
@@ -1,3 +0,0 @@
-package net.onelitefeather.blackhole.util
-
-const val PERMANENT_IDENTIFIER = 1_000.0
