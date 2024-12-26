740ccfc89bf7c36721afb010727fcad87db76234
theEvilReaper <steffenwx@gmail.com>
2024-12-26 17:13:52 +0100

    Add dependency resolution management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,46 @@
 rootProject.name = "Blackhole"
+
+dependencyResolutionManagement {
+    if (System.getenv("CI") != null) {
+        repositoriesMode = RepositoriesMode.PREFER_SETTINGS
+        repositories {
+            maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            maven("https://repo.htl-md.schule/repository/Gitlab-Runner/")
+            maven {
+                val groupdId = 28 // Gitlab Group
+                val ciApiv4Url = System.getenv("CI_API_V4_URL")
+                url = uri("$ciApiv4Url/groups/$groupdId/-/packages/maven")
+                name = "GitLab"
+                credentials(HttpHeaderCredentials::class.java) {
+                    name = "Job-Token"
+                    value = System.getenv("CI_JOB_TOKEN")
+                }
+                authentication {
+                    create<HttpHeaderAuthentication>("header")
+                }
+            }
+        }
+    } else {
+        repositories {
+            mavenCentral()
+            maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            maven {
+                val groupdId = 28 // Gitlab Group
+                url = uri("https://gitlab.onelitefeather.dev/api/v4/groups/$groupdId/-/packages/maven")
+                name = "GitLab"
+                credentials(HttpHeaderCredentials::class.java) {
+                    name = "Private-Token"
+                    value = providers.gradleProperty("gitLabPrivateToken").get()
+                }
+                authentication {
+                    create<HttpHeaderAuthentication>("header")
+                }
+            }
+        }
+    }
+    versionCatalogs {
+        create("libs") {
+            //TODO: Add content
+        }
+    }
+}
