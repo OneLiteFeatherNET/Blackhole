e646df385d2509daa7ecf5f39813b898e05b0e0e
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2024-12-28 19:01:17 +0100

    Remove client based id generation

--- api/src/main/java/net/onelitefeather/blackhole/api/template/PunishTemplateBuilder.java
+++ api/src/main/java/net/onelitefeather/blackhole/api/template/PunishTemplateBuilder.java
@@ -39,18 +39,6 @@ final class PunishTemplateBuilder implements PunishTemplate.Builder {
     }
 
     @Override
-    public PunishTemplate.Builder identifier(@NotNull UUID identifier) {
-        this.identifier = identifier;
-        return this;
-    }
-
-    @Override
-    public PunishTemplate.Builder generateIdentifier() {
-        this.identifier = UUID.randomUUID();
-        return this;
-    }
-
-    @Override
     public PunishTemplate.Builder translatable() {
         this.metaData.put(PunishTemplate.META_DATA_KEY_TRANSLATABLE, true);
         return this;
@@ -72,9 +60,6 @@ final class PunishTemplateBuilder implements PunishTemplate.Builder {
     public PunishTemplate build() {
         if (this.type == null) {
             throw new IllegalStateException("The type must be set");
-        }
-        if (this.identifier == null) {
-            throw new IllegalStateException("The identifier must be set");
         }
         if (this.reason == null) {
             throw new IllegalStateException("The reason must be set");
