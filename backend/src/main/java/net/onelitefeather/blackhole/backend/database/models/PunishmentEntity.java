5bfe8a1ff9d0400450053e6dfa332256158388b4
Steffen <theevilreaper@onelitefeather.net>
2024-12-28 00:16:16 +0000

    Add controller for the profiles interaction

--- backend/src/main/java/net/onelitefeather/blackhole/backend/database/models/PunishmentEntity.java
+++ backend/src/main/java/net/onelitefeather/blackhole/backend/database/models/PunishmentEntity.java
@@ -2,9 +2,12 @@ package net.onelitefeather.blackhole.backend.database.models;
 
 import io.micronaut.serde.annotation.Serdeable;
 import jakarta.persistence.*;
+import net.onelitefeather.blackhole.api.punish.PunishEntry;
 import net.onelitefeather.blackhole.api.punish.PunishType;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 @Serdeable
@@ -19,35 +22,122 @@ public class PunishmentEntity {
 
     private UUID source;
 
+    @OneToOne
+    private PunishmentTemplateEntity template;
+
     @ElementCollection
     @MapKeyColumn(name = "key")
     @Column(name = "value")
     @CollectionTable(name = "punishments_metadata", joinColumns = @JoinColumn(name = "identifier"))
     private Map<String, Object> metaData;
 
+    /**
+     * Convert a PunishEntry to a PunishmentEntity.
+     *
+     * @param entry the PunishEntry to convert
+     * @return the converted PunishmentEntity
+     */
+    public static PunishmentEntity toEntity(PunishEntry entry) {
+        return new PunishmentEntity(
+                entry.identifier(),
+                entry.type(),
+                entry.source(),
+                PunishmentTemplateEntity.toEntity(entry.template()),
+                entry.metaData()
+        );
+    }
+
+    public static PunishmentEntity toEntity(Optional<PunishEntry> entityOptional) {
+        return entityOptional.map(PunishmentEntity::toEntity).orElse(null);
+    }
+
+    public static List<PunishmentEntity> toEntities(List<PunishEntry> entries) {
+        return entries.stream().map(PunishmentEntity::toEntity).toList();
+    }
+
     public PunishmentEntity() {
     }
 
-    public PunishmentEntity(String identifier, PunishType type, UUID source, Map<String, Object> metaData) {
+    /**
+     * Create a new PunishmentEntity with a template.
+     *
+     * @param identifier the identifier of the punishment
+     * @param type       the type of the punishment
+     * @param source     the source of the punishment
+     * @param metaData   the metadata of the punishment
+     */
+    public PunishmentEntity(String identifier, PunishType type, UUID source, PunishmentTemplateEntity template, Map<String, Object> metaData) {
         this.identifier = identifier;
         this.type = type;
         this.source = source;
+        this.template = template;
         this.metaData = metaData;
     }
 
+    /**
+     * Set's the template of the punishment.
+     *
+     * @param template the template to set
+     */
+    public void setTemplate(PunishmentTemplateEntity template) {
+        this.template = template;
+    }
+
+    /**
+     * Get the identifier of the punishment.
+     *
+     * @return the identifier
+     */
     public String getIdentifier() {
         return identifier;
     }
 
+    /**
+     * Get the type of the punishment.
+     *
+     * @return the type
+     */
     public PunishType getType() {
         return type;
     }
 
+    /**
+     * Get the source of the punishment.
+     *
+     * @return the source
+     */
     public UUID getSource() {
         return source;
     }
 
+    /**
+     * Get the template of the punishment.
+     *
+     * @return the template
+     */
+    public PunishmentTemplateEntity getTemplate() {
+        return template;
+    }
+
+    /**
+     * Get the metadata of the punishment.
+     *
+     * @return the metadata
+     */
     public Map<String, Object> getMetaData() {
         return metaData;
+    }
+
+    /**
+     * Convert a PunishmentEntity to a PunishEntry
+     *
+     * @return the converted PunishEntry
+     */
+    public PunishEntry toDTO() {
+        return PunishEntry.builder()
+                .source(source)
+                .type(type)
+                .template(template.toDTO())
+                .build();
     }
 }
--- backend/src/main/java/net/onelitefeather/blackhole/backend/database/models/PunishmentProfileEntity.java
+++ backend/src/main/java/net/onelitefeather/blackhole/backend/database/models/PunishmentProfileEntity.java
@@ -2,6 +2,7 @@ package net.onelitefeather.blackhole.backend.database.models;
 
 import io.micronaut.serde.annotation.Serdeable;
 import jakarta.persistence.*;
+import net.onelitefeather.blackhole.api.profile.PunishProfile;
 
 import java.util.List;
 import java.util.Map;
@@ -29,9 +30,38 @@ public class PunishmentProfileEntity {
     @CollectionTable(name = "punishment_profiles_metadata", joinColumns = @JoinColumn(name = "owner"))
     private Map<String, Object> metaData;
 
-    public PunishmentProfileEntity() {
+    /**
+     * Convert a PunishProfile to a PunishmentProfileEntity.
+     *
+     * @param profile the PunishProfile to convert
+     * @return the converted PunishmentProfileEntity
+     */
+    public static PunishmentProfileEntity toEntity(PunishProfile profile) {
+        return new PunishmentProfileEntity(
+                profile.owner(),
+                PunishmentEntity.toEntity(profile.activeChatBan()),
+                PunishmentEntity.toEntity(profile.activeBan()),
+                PunishmentEntity.toEntities(profile.history()),
+                profile.metaData()
+        );
     }
 
+    /**
+     * Create a new PunishmentProfileEntity.
+     */
+    public PunishmentProfileEntity() {
+        // Default constructor for JPA
+    }
+
+    /**
+     * Create a new PunishmentProfileEntity.
+     *
+     * @param owner         the owner of the profile
+     * @param activeChatBan the active chat ban
+     * @param activeBan     the active ban
+     * @param history       the history
+     * @param metaData      the metadata
+     */
     public PunishmentProfileEntity(
             String owner,
             PunishmentEntity activeChatBan,
@@ -46,23 +76,80 @@ public class PunishmentProfileEntity {
         this.metaData = metaData;
     }
 
+    /**
+     * Set the active ban of the profile.
+     *
+     * @param activeBan the active ban
+     */
+    public void setActiveBan(PunishmentEntity activeBan) {
+        this.activeBan = activeBan;
+    }
+
+    /**
+     * Set the active chat ban of the profile.
+     *
+     * @param activeChatBan the active chat ban
+     */
+    public void setActiveChatBan(PunishmentEntity activeChatBan) {
+        this.activeChatBan = activeChatBan;
+    }
+
+    /**
+     * Get the owner of the profile.
+     *
+     * @return the owner
+     */
     public String getOwner() {
         return owner;
     }
 
+    /**
+     * Get the active ban of the profile.
+     *
+     * @return the active ban
+     */
     public PunishmentEntity getActiveBan() {
         return activeBan;
     }
 
+    /**
+     * Get the active chat ban of the profile.
+     *
+     * @return the active chat ban
+     */
     public PunishmentEntity getActiveChatBan() {
         return activeChatBan;
     }
 
+    /**
+     * Get the history of the profile.
+     *
+     * @return the history
+     */
     public List<PunishmentEntity> getHistory() {
         return history;
     }
 
+    /**
+     * Get the metadata of the profile.
+     *
+     * @return the metadata
+     */
     public Map<String, Object> getMetaData() {
         return metaData;
+    }
+
+    /**
+     * Convert the entity to a DTO.
+     *
+     * @return the created to variant
+     */
+    public PunishProfile toDTO() {
+        return PunishProfile.builder()
+                .owner(this.owner)
+                .metaData(this.metaData)
+                .activeChatBan(this.activeChatBan.toDTO())
+                .activeBan(this.activeBan.toDTO())
+                .build();
     }
 }
