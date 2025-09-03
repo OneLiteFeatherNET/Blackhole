40995eb60d919a9b46d4ce2f5071f0cc60a3b6b1
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-09-03 15:38:51 +0200

    Improve formatting

--- backend/src/main/java/net/onelitefeather/blackhole/backend/BlackholeApplication.java
+++ backend/src/main/java/net/onelitefeather/blackhole/backend/BlackholeApplication.java
@@ -14,7 +14,11 @@ import io.swagger.v3.oas.annotations.info.License;
                 version = "0.0.1",
                 description = "Simple ban management system",
                 license = @License(name = "Close Source"),
-                contact = @Contact(url = "https://onelitefeather.net", name = "Management", email = "admin@onelitefeather.net")
+                contact = @Contact(
+                        url = "https://onelitefeather.net",
+                        name = "Management",
+                        email = "admin@onelitefeather.net"
+                )
         )
 )
 public final class BlackholeApplication {
