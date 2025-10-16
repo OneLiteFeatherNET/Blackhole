371ce06bec48f154cdb142e5eb7c08a09f776b49
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-10-16 13:55:09 +0200

    Add missing platform launcher

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -7,11 +7,13 @@ plugins {
 }
 
 dependencies {
+    implementation(platform(mn.micronaut.core.bom))
     implementation(libs.jetbrains.annotations)
     implementation(project(":phoca"))
 
     testImplementation(project(":phoca"))
     testImplementation(mn.junit.jupiter.api)
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testRuntimeOnly(mn.junit.jupiter.engine)
 }
 
